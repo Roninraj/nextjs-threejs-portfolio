c411889d768bf62e2703f6e9db5a78274b8d0c7a
Rahul Raj Soren <rahulsoren90@gmail.com>
2022-06-18 14:39:33 +0530

    Update section.js

--- components/section.js
+++ components/section.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 import { chakra, shouldForwardProp  } from "@chakra-ui/react"
-import { Children } from "react"
+//import { children } from "react"
 
 const StyledDiv = chakra(motion.div, {
     shouldForwardProp: prop => {
@@ -14,7 +14,7 @@ const Section = ({children, delay=0}) => (
         animate={{y:0, opacity:1}}
         transition={{duration:0.8,delay}}
         mb={6}>
-            {/* //{children} */}
+            {children}
     </StyledDiv>
 )
 
