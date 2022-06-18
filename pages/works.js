eceb27cce5007520f017c5e44ce9d29840c433c4
Rahul Raj Soren <rahulsoren90@gmail.com>
2022-06-18 16:05:09 +0530

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -1,12 +1,19 @@
-// import { Container, Box, Heading, SimpleGrid, Divider} from "@chakra-ui/react";
-// import Section from "../components/section";
+import { Container, Box, Heading, SimpleGrid, Divider} from "@chakra-ui/react";
+import Section from "../components/section";
 
-// const Works = () =>{
-// return(
-//     <Container>
-//         <Heading as="h3" fontSize={20} mb={4}>
-//             Works
-//         </Heading>
-//     </Container>
-// )
-// }
+const Works = () =>{
+return(
+    <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+            Works
+        </Heading>
+        <Section>
+            <Box></Box>
+        <SimpleGrid></SimpleGrid>
+        <Divider></Divider>
+        </Section>
+    </Container>
+)
+}
+
+export default Works
