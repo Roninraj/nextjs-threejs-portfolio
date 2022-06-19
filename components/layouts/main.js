48efa242e7453da03d4d48ab167669c1e0313823
Rahul Raj Soren <rahulsoren90@gmail.com>
2022-06-19 16:20:59 +0530

    loader

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '../navbar'
 import {Box, Container} from '@chakra-ui/react'
 import VoxelFox from '../voxel-fox'
+import NoSsr from '../no-ssr'
 const Main = ({children, router}) => {
     return(
         <Box as="main" pb={8}>
@@ -13,7 +14,9 @@ const Main = ({children, router}) => {
             <Navbar path={router.asPath}/>
 
             <Container maxW="container.md" pt={14}>
-                <VoxelFox/>
+                <NoSsr>
+                    <VoxelFox/>
+                </NoSsr>
                 {children}
             </Container>
         </Box>
