8fbfd56f2ff95fa46eeeffa427e55c534b104339
Rahul Raj Soren <rahulsoren90@gmail.com>
2022-06-19 15:09:14 +0530

    Update voxel-fox.js

--- components/voxel-fox.js
+++ components/voxel-fox.js
@@ -1,12 +1,12 @@
-import { useState, useEffect, useRef, useCallback } from "react"
-import { Box, Spinner } from "@chakra-ui/react"
-import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import {loadGLTFModel} from '../lib/model'
+//import { useState, useEffect, useRef, useCallback } from "react"
+import { Box, } from "@chakra-ui/react"
+//import * as THREE from 'three'
+//import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+//import {loadGLTFModel} from '../lib/model'
 
-function easeOutCirc(x) {
-    return Math.sqrt(1-Math.pow(x-1,4))
-}
+// function easeOutCirc(x) {
+//     return Math.sqrt(1-Math.pow(x-1,4))
+// }
 
 // const VoxelFox = () => {
 //     const refContainer = useRef()
